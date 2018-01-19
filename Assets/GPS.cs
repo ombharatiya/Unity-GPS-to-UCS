628d1bbea3af93d6b1e5284b3a186d69b824b467
ombharatiya <om.dkcarmel@gmail.com>
2018-01-20 01:36:36 +0530

    Second Commit

---
+++ Assets/GPS.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPS : MonoBehaviour {
+    public static GPS Instance { set; get; }
+    public float latitude;
+    public float longitude;
+	// Use this for initialization
+	private void Start () {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        StartCoroutine(StartLocationService());
+	}
+
+    private IEnumerator StartLocationService()
+    {
+        if (!Input.location.isEnabledByUser)
+        {
+            Debug.Log("User has not enabled gps");
+            yield break;
+        }
+        Input.location.Start();
+        int maxWait = 20;
+        while(Input.location.status==LocationServiceStatus.Initializing && maxWait>0){
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+        if (maxWait <= 0)
+        {
+            Debug.Log("Timed Out");
+            yield break;
+        }
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            Debug.Log("Unable to determine device location");
+            yield break;
+        }
+
+        latitude = Input.location.lastData.latitude;
+        longitude = Input.location.lastData.longitude;
+        yield break;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        latitude = Input.location.lastData.latitude;
+        longitude = Input.location.lastData.longitude;
+    }
+}
---
+++ Assets/updategpstext.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class updategpstext : MonoBehaviour {
+    public Text coordinates;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        coordinates.text = "LAT:" + GPS.Instance.latitude.ToString() + "  LON:" + GPS.Instance.longitude.ToString()+"\nGPSToUCS:" + GPStoUCS.GPSEncoder.GPSToUCS(GPS.Instance.latitude, GPS.Instance.longitude).ToString();
+            }
+}
